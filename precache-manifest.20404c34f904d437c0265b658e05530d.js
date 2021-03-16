756b5bc4580e371d63b4b07be948f7a9d6e61624
gustavo Cogollo <gac982@gmail.com>
2021-03-16 08:28:46 -0400

    upload

---
+++ precache-manifest.20404c34f904d437c0265b658e05530d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294d505ca1f6b6de5606",
+    "url": "/css/app.2d595bd0.css"
+  },
+  {
+    "revision": "16f6db4a365a31dea980f75f9b773e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "294d505ca1f6b6de5606",
+    "url": "/js/app.06abd2d8.js"
+  },
+  {
+    "revision": "8035471c30466fdefab0",
+    "url": "/js/chunk-vendors.e34dd964.js"
+  },
+  {
+    "revision": "d1192ee39b9dc18785ec4af6eadbb8cc",
+    "url": "/manifest.json"
+  }
+]);
